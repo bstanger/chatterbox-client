75d820fbc94b076e0c6388cf06529cc28f4276cf
bstanger <beccastanger@gmail.com>
2018-01-27 12:05:24 -0800

    Add top rooms to dropdown

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
   $('.input-message_btn').on('click', function() {
     var message = $('.input-message').val();
-    makeApp.prototype.send(message);
+    makeApp.prototype.createMessageObj(message);
   });
 
 });
@@ -13,49 +13,78 @@ var makeApp = function() {
   this.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
 };
 
-makeApp.prototype.init = function() {
-  this.fetch();
+makeApp.prototype.init = () => {
+  makeApp.prototype.fetch(makeApp.prototype.populateDropDown);
   setInterval(function () {
     makeApp.prototype.fetch.call(this);
   }, 60000);
 };
 
-makeApp.prototype.fetch = function () {
+makeApp.prototype.fetch = cb => {
   var allChats;
+  var chatsByRoom = {};
+  var makeAppContext = makeApp;
   $.ajax({
     type: "GET",
     url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
     success: function (response) {
       allChats = response.results;
       _.each(allChats, function (item) {
+        var roomname = item.roomname;
+        var roomObj = {};
+        if (chatsByRoom[roomname]) {
+          chatsByRoom[roomname].push(item);
+        } else {
+          chatsByRoom[roomname] = [item];
+        }
         this.prototype.displayChat(item);
-      }, makeApp);    
+      }, makeApp); 
+      makeAppContext.chatsByRoom = chatsByRoom;
+      if (cb && typeof cb === 'function') {
+        cb();
+      }   
     }
   });
+};
+
+makeApp.prototype.populateDropDown = () => {
+  console.log('test');
+  for (var key in makeApp.chatsByRoom) {
+    var numberChatsInRoom = makeApp.chatsByRoom[key].length;
+    if (numberChatsInRoom > 4) {
+      if (key !== ""){
+        let $option = $(`<option value="${key}">${key}</option>`);
+        $('.chatroom-dropdown').append($option);
+      }
+    }
+  }
 };
 
 makeApp.prototype.displayChat = chat => {
   console.log(chat);
   var {username, text} = chat;
-  username = validator.escape("" + username);
-  text = validator.escape("" + text);
+  username = _.escape(username);
+  text = _.escape(text);
   let $message = $(`<div class ="message"></div>`);
   $message.append($(`<h3 class="message_username">${username}</h3>`));
   $('.message_username').append($(`<div class="message_text">${text}</div>`));
   $('#chats').prepend($message);
 };
 
-makeApp.prototype.send = message => {
+makeApp.prototype.createMessageObj = message => {
   var dataObj = {};
-  // message = validator.escape(message);
-  dataObj.username = '';
+  dataObj.username = decodeURI(window.location.search.split('?username=')[1]);
   dataObj.text = message;
   dataObj.roomname = '';
+  this.send(messageObj);
+};
+
+makeApp.prototype.send = messageObj => {
   $.ajax({
     type: 'POST',
     contentType: 'application/json',
     url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
-    data: JSON.stringify(dataObj),
+    data: JSON.stringify(messageObj),
     success: function(response){
       console.log("Success!");
     }
